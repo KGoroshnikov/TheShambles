34ba91ee26029f1d7863d1126d7f858c7c2c3fca
Alexander1248 <sasha,izmyloff@mail.ru>
2023-12-01 16:49:30 +0300

    Chase and Save

--- Assets/Scripts_Alexander/Checkpoint.cs
+++ Assets/Scripts_Alexander/Checkpoint.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using UnityEditor.SearchService;
 using UnityEngine;
 
@@ -16,8 +17,8 @@ public class Checkpoint : MonoBehaviour
     {
         Debug.Log(transform.position + " - " + checkPoints.Count);
 
-        if (checkPoints.Count == lastCheckpoint) 
-            checkpointGained = true;
+        if (checkPoints.Count == lastCheckpoint)
+            SaveState();
 
         checkPoints.Add(this);
 
@@ -29,15 +30,46 @@ public class Checkpoint : MonoBehaviour
     {
         if (checkpointGained) return;
         if (other.tag != "Player") return;
+        SaveState();
+    }
 
+    private void SaveState()
+    {
         Debug.Log("Triggered!");
         lastCheckpoint = checkPoints.IndexOf(this);
         checkpointGained = true;
+
+        Dictionary<string, ObjectState> states = new Dictionary<string, ObjectState>();
+
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+            states.Add(go.name, new ObjectState(go.transform.position, go.transform.rotation));
+        
+        File.WriteAllText("save.gd", JsonUtility.ToJson(states));
     }
 
-    public static void LoadLastCheckpoint(MonoBehaviour obj)
+    public static void LoadLastCheckpoint()
     {
-        obj.transform.position = checkPoints[lastCheckpoint].transform.position;
-        // TODO: Return game state checkpoint
+        Dictionary<string, ObjectState> states = JsonUtility.FromJson<Dictionary<string, ObjectState>>(File.ReadAllText("save.gd"));
+
+        foreach(string name in states.Keys)
+        {
+            Transform transform = GameObject.Find(name).transform;
+            transform.position = states[name].position;
+            transform.rotation = states[name].rotation;
+        }
+    }
+
+    [System.Serializable]
+    private class ObjectState
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public ObjectState(Vector3 position, Quaternion rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
     }
 }
---
+++ Assets/Scripts_Alexander/EnemyDog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyDog : MonoBehaviour
+{
+    public Transform goal;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player") return;
+        Checkpoint.LoadLastCheckpoint();
+    }
+}
